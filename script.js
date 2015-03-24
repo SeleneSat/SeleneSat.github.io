0eac027757fc4f71d56f0140d61333e5bfdf6753
Nodrokov <nodrokov@gmail.com>
2015-03-24 12:04:08 -0700

    get menu working

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-$(document).ready(main);
-function main(){
-$("#openmenu").click(function(){
+function main()
+{
+$("#openmenu").click(function(){$("#sidebar").css("left","0px");});
+$("#close_menu").click(function(){$("#sidebar").css("left","-200px");});
+}
+$(window).load(main);
