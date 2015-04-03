edee632e484d53e0a638d419ad2bd6cfa8daa340
Isaac Shure <nodrokov@gmail.com>
2015-04-02 19:45:39 -0700

    minor tweaks to make opacity look better

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function main()
 {
-$("#openmenu").click(function(){$("#sidebar").css("left","0px");});
-$("#close_menu").click(function(){$("#sidebar").css("left","-200px");});
+$("#openmenu").click(function(){$("#sidebar").css("left","0px");
+$("#openmenu").css("left","-300px");});
+$("#close_menu").click(function(){$("#sidebar").css("left","-200px");
+$("#openmenu").css("left","auto");});
 }
 $(window).load(main);
