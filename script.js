011c682bb29e52f14047b5f19722dea78e78ad51
Isaac Shure <nodrokov@gmail.com>
2015-04-02 21:34:32 -0700

    fix teeny bug

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function main()
 $("#openmenu_container").click(function(){$("#sidebar").css("left","0px");
 $("#openmenu_container").css("left","-300px");});
 $("#close_menu").click(function(){$("#sidebar").css("left","-200px");
-$("#openmenu_container").css("left","auto");});
+$("#openmenu_container").css("left","40px");});
 }
 $(window).load(main);
