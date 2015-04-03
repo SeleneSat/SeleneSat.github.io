b4499b4e69c371cb4ac1a7aca5031e8117dfe40b
Isaac Shure <nodrokov@gmail.com>
2015-04-02 22:04:55 -0700

    fix spelling error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function main()
 {
-  $("#openmenu_container").mousenter(function(){
+  $("#openmenu_container").mouseenter(function(){
 $("#sidebar").css("left","0px");
 $("#openmenu_container").css("left","-200px");
 $("#sidebar").mouseleave(function(){
