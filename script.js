a947b0637bbdae1db940d6d750f7719687717a50
Isaac Shure <nodrokov@gmail.com>
2015-04-02 22:56:56 -0700

    sigh, I'll get this branch working later

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function main()
 {
-  $("#openmenu_container").mouseenter(function (){
-$("#sidebar").css("left","0px");
-$("#openmenu_container").css("left","-200px"););
-  $("#sidebar").mouseleave(function (){
-$("#sidebar").css("left","-200px");
-$("#openmenu_container").css("left","40px");});
+console.log("document ready, Javascript running");
+  $(".openmenu_container").mouseover(function mouseoverfunc (){
+console.log("mouseover triggered");
+$(".sidebar").css("left","0px");
+$(".openmenu_container").css("left","-200px"););
+  $(".sidebar").mouseleave(function mouseoutfunc (){
+console.log("mouseout triggered");
+$(".sidebar").css("left","-200px");
+$(".openmenu_container").css("left","40px");});
 }
-$(window).load(main);
+}
+$(document).ready(main);
