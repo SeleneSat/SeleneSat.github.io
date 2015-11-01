676d4a5456d6bdb9de91884a2ed19768246350f9
Isaac Shure <nodrokov@gmail.com>
2015-11-01 12:21:36 -0800

    i don't even know what this is

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ console.log("document ready, Javascript running");
   $(".openmenu_container").mouseover(function mouseoverfunc (){
 console.log("mouseover triggered");
 $(".sidebar").css("left","0px");
-$(".openmenu_container").css("left","-200px"););
+$(".openmenu_container").css("left","-200px");});
   $(".sidebar").mouseleave(function mouseoutfunc (){
 console.log("mouseout triggered");
 $(".sidebar").css("left","-200px");
 $(".openmenu_container").css("left","40px");});
-}
 }
 $(document).ready(main);
